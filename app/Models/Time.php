9f50df969e96703bdcec8f7b99fd3f45973fb5cd
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2025-04-18 06:59:41 +0700

    update
    appointment

--- app/Models/Time.php
+++ app/Models/Time.php
@@ -8,15 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     use HasFactory;
-    
+
     protected $keyType = 'string';
     public $incrementing = false;
-    
+
     protected $fillable = [
         'id',
         'started_time',
+        'capacity',
+        'booked_count',
     ];
-    
+
+    /**
+     * Check if the time slot is full
+     *
+     * @return bool
+     */
+    public function isFull()
+    {
+        return $this->booked_count >= $this->capacity;
+    }
+
+    /**
+     * Get the number of available slots
+     *
+     * @return int
+     */
+    public function getAvailableSlotsAttribute()
+    {
+        return max(0, $this->capacity - $this->booked_count);
+    }
+
     public function appointments()
     {
         return $this->hasMany(Appointment::class, 'time_appointments_id');
